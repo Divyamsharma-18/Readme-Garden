f94960913ea62ce3b974d437c80cb92aa7a7c590
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-09-03 09:02:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 
+import { Analytics } from "@vercel/analytics/next";
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
